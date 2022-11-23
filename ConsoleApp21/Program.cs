781739b5b2d5c68025c39b288e82c5a15ee2fbf5
Elena <egurnovaelena56@gmail.com>
2022-11-23 14:47:40 +0500

    1 rj'kdg;sd

--- ConsoleApp21/Program.cs
+++ ConsoleApp21/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace ConsoleApp21
+﻿using System.Globalization;
+
+namespace ConsoleApp21
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //df;lkgtd\s;fkjhg\fdl;jkh\
+           
         }
     }
 }
